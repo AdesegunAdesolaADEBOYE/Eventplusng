290761208847c19c50c36456149efab4c8305945
Adesegun Adesola ADEBOYE <fantasticinkmedia@gmail.com>
2022-01-28 05:10:10 +0100

    Updated Settings.py serializers.py views.py etc.

--- Eventplusng/Eventplus/settings.py
+++ Eventplusng/Eventplus/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'event',
     'event_api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- Eventplusng/event_api/urls.py
+++ Eventplusng/event_api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from .views import PostList, PostDetail
 
 
 app_name = 'event_api'
 
 urlpatterns = [
-
+    path('int:pk>/', PostDetail.as_view(), name='detailcreate'),
+    path('', PostList.as_view(), name='listcreate'),
 ]
 
